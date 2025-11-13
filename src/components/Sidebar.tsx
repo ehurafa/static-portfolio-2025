22d9fe5e401dd7fa2bcc5f06a2ece61848bef9a3
Rafael Gomes <rafarfl2010@gmail.com>
2025-11-12 23:52:14 -0300

    feat(styles): fixing sidebar styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,16 +2,17 @@ import { NavLink } from 'react-router-dom';
 import { TiSocialLinkedin } from 'react-icons/ti';
 import { BiLogoPinterestAlt } from 'react-icons/bi';
 import { FaGithubAlt } from 'react-icons/fa6';
-import profileImg from '../assets/profile.jpg';
+import profileImage from '../assets/profile.jpg';
+import brandImage from '../assets/brand.png';
 
 export default function Sidebar() {
   return (
     <div>
       <div className="avatar">
-        <img src={profileImg} alt="Profile photo" />
+        <img src={profileImage} alt="Foto de perfil" />
       </div>
-      <h1>
-        Rafael<strong>Gomes</strong>
+      <h1 className="brand">
+        <img src={brandImage} alt="Rafael Gomes" />
       </h1>
       <span className="role">Front-end Developer</span>
       <div className="social">
